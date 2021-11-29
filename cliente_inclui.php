498ff750762579d195cc2d09f55eaf597049df9b
ggdeutsch <gunther.goemann@gmail.com>
2021-11-29 09:52:06 -0300

    Add files via upload

--- cliente_inclui.php
+++ cliente_inclui.php
@@ -21,6 +21,8 @@ window.moveTo(x,y);
 
 </script>
 
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+
 <script language="javascript">
 
 function validar(formulario) 
@@ -100,6 +102,109 @@ function SomenteNumeroMesmo()  //so numeros mesmo
  }  
 }
 
+
+
+/*
+CPF e CNPJ - Inicio
+*/
+
+function validar(obj) { // recebe um objeto
+    var s = (obj.value).replace(/\D/g, '');
+    var tam = (s).length; // removendo os caracteres nãoo numéricos
+    if (!(tam == 11 || tam == 14 || tam == 0)) { // validando o tamanho
+        alert("'" + s + "' Não é um CPF ou um CNPJ Válido!"); // tamanho inválido
+        obj.focus();
+        return false;
+    }
+
+// se for CPF
+    if (tam == 11) {
+        if (!validaCPF(s)) { // chama a função que valida o CPF
+            alert("'" + s + "' Não é um CPF Válido!"); // se quiser mostrar o erro
+            obj.select(); // se quiser selecionar o campo em questão
+            obj.focus();
+            return false;
+        }
+        obj.value = maskCPF(s); // se validou o CPF mascaramos corretamente
+        return true;
+    }
+
+// se for CNPJ
+    if (tam == 14) {
+        if (!validaCNPJ(s)) { // chama a função que valida o CNPJ
+            alert("'" + s + "' Não é um CNPJ Válido!"); // se quiser mostrar o erro
+            obj.select(); // se quiser selecionar o campo enviado
+            return false;
+        }
+        obj.value = maskCNPJ(s); // se validou o CNPJ mascaramos corretamente
+        return true;
+    }
+}
+
+function validaCPF(s) {
+    var c = s.substr(0, 9);
+    var dv = s.substr(9, 2);
+    var d1 = 0;
+    for (var i = 0; i < 9; i++) {
+        d1 += c.charAt(i) * (10 - i);
+    }
+    if (d1 == 0)
+        return false;
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9)
+        d1 = 0;
+    if (dv.charAt(0) != d1) {
+        return false;
+    }
+    d1 *= 2;
+    for (var i = 0; i < 9; i++) {
+        d1 += c.charAt(i) * (11 - i);
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9)
+        d1 = 0;
+    if (dv.charAt(1) != d1) {
+        return false;
+    }
+    return true;
+}
+
+function validaCNPJ(CNPJ) {
+    var a = new Array();
+    var b = new Number;
+    var c = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    for (i = 0; i < 12; i++) {
+        a[i] = CNPJ.charAt(i);
+        b += a[i] * c[i + 1];
+    }
+    if ((x = b % 11) < 2) {
+        a[12] = 0
+    } else {
+        a[12] = 11 - x
+    }
+    b = 0;
+    for (y = 0; y < 13; y++) {
+        b += (a[y] * c[y]);
+    }
+    if ((x = b % 11) < 2) {
+        a[13] = 0;
+    } else {
+        a[13] = 11 - x;
+    }
+    if ((CNPJ.charAt(12) != a[12]) || (CNPJ.charAt(13) != a[13])) {
+        return false;
+    }
+    return true;
+}
+function maskCPF(CPF) {
+    return CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "." + CPF.substring(6, 9) + "-" + CPF.substring(9, 11);
+}
+function maskCNPJ(CNPJ) {
+    return CNPJ.substring(0, 2) + "." + CNPJ.substring(2, 5) + "." + CNPJ.substring(5, 8) + "/" + CNPJ.substring(8, 12) + "-" + CNPJ.substring(12, 14);
+}
+/*
+CPF e CNPJ -FIM
+*/
 </script>
 
 <link href="./css/estilos.css" rel="stylesheet" type="text/css">
@@ -221,6 +326,12 @@ if(isset($_POST['acao']))
 			</tr>
 
 			<tr>
+			  <td class="azul14"> CPF / CNPJ:</td>
+			  <td align="left" class="laranja14">
+			  <input name="cpf_cnpj" id="cpf_cnpj" type="text" size="15" maxlength="15" class="laranja14" onblur="validar(this)">
+			</tr>
+
+			<tr>
 			  <td class="azul14"> Já é Sócio ?</td>
 			  <td align="left" class="laranja14">
 				<select name="socio" id="socio" class="laranja14">
@@ -228,12 +339,6 @@ if(isset($_POST['acao']))
 				<option value="N"> Não</option>
 				</select>
 			</td>
-			</tr>
-
-			<tr>
-			  <td class="azul14"> CPF / CNPJ:</td>
-			  <td align="left" class="laranja14">
-			  <input name="cpf_cnpj" id="cpf_cnpj" type="text" size="15" maxlength="15" class="laranja14">
 			</tr>
 
 			<tr>
